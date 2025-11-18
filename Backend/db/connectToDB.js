9d8de80b4ef6015cb2410dd7700980239fdfd7b3
muhammadfurqanrasool <muhammadfurqanrasool@gmail.com>
2025-11-19 00:14:42 +0500

    My first Commit

--- Backend/db/connectToDB.js
+++ Backend/db/connectToDB.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export default async function connectToDB() {
+    // Hello Hasnain!
     return mongoose.connect(process.env.MONGO_URI).then(()=> console.log("Connected to DB!")).catch(e=> console.log(e));
 }
